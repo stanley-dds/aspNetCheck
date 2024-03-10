c3134351a005a7d444cbcc4d7889f81dc1741562
stanley-dds <117214544+stanley-dds@users.noreply.github.com>
2024-03-10 15:45:48 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplicationSunday.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// adding services to container 
+builder.Services.AddControllersWithViews();
+
+// adding  context of the database
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
